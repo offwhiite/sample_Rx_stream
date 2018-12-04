aef4a7a27f559699facef5cd704a3b45d2ace13b
off2white <dev.off.white@gmail.com>
2018-12-04 20:27:09 +0900

    init commit

---
+++ app/src/main/java/offwhite/sample_rx_stream/MainActivity.kt
@@ -0,0 +1,12 @@
+package offwhite.sample_rx_stream
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/offwhite/sample_rx_stream/SampleStream.kt
@@ -0,0 +1,77 @@
+package offwhite.sample_rx_stream
+
+import io.reactivex.Maybe
+import io.reactivex.MaybeOnSubscribe
+import io.reactivex.Observable
+import io.reactivex.Single
+
+/**
+ * ストリーム作成のサンプル
+ *
+ * ・ストリームの作成
+ *     ・Observable
+ *     ・Single
+ *     ・Flowable
+ *     ・Maybe
+ * ・データソースの設定
+ *     ・fromArray
+ *     ・create
+ *     ・just
+ */
+class SampleStream {
+
+    /**
+     * サンプル１：リストのストリームを生成する
+     */
+    fun getStringListStreamFromObservable() : Observable<Array<String>>{
+
+        // Observable はストリームを配置することができます。
+        // fromArrayはこのストリームにデータソースを設定しています。
+        //  データソースは、ストリームに流れるデータの元になルものです。
+        //  APIのレスポンスやDBに保存されているデータなどが設定される事が多いかと思います。
+        // データソースの設定方法は複数あるが、配列をデータソースとしたい場合はfromArrayを用います。
+        // 注：サンプルの多くはfromメソッドを利用しているが、
+        // 2018/12/04時点ではfrom関数がJavadocに記載されていない
+        return Observable.fromArray(arrayOf("a","b","c"))
+    }
+
+    /**
+     * サンプル２：リストのストリームを生成する(Single)
+     */
+    fun getStringListStreamFromSingle() : Single<String> {
+
+        // SingleもObservableと同様にストリームを配置する事ができますが、
+        // Observableと以下の点が異なります。
+        // 1.単一の値しか流せない(配列は流せない)
+        // 2.一度イベントが発生した後は、購読されない。(onSuccess, onErrorどちらか一つ発生したら終了)
+        // そのため、基本的にはAPIのレスポンスクラス等の”クラス一つだけを一回取りに行く”ようなHttp通信時に用いると思います。
+        // justは単一の値(item)をSingleのストリームに流します。(流す事をemitと呼んだりします。)
+        return Single.just("Hello")
+    }
+
+
+    /**
+     * サンプル３：リストのストリームを生成する(Maybe)
+     */
+    fun getStringListStreamFromMaybe() : Maybe<String> {
+
+        // MaybeはSingleとほぼ同じです。
+        // しかしSingleと異なり、"値を返さずに終了しても良い"点が異なります。
+        // createは公式の説明だと、”リアクティブの世界とコールバックの世界を橋渡しするAPI” とあります。
+        // つまりストリームの情報から判断して適切なコールバックを呼び出したい時に使うと思っています。
+        // リアクティブプログラミングをしたい場合は基本的にこのcreateを使う事が多いかなと思います。
+        return Maybe.create { emitter->
+            val result = "no data"
+            if ("no data" == result) {
+                // Singleだとできないが、ここで値を流さずに終了ができる
+                emitter.onComplete()
+            } else if ("has data" == result) {
+                // Singleと同じように値を返す
+                emitter.onSuccess(result)
+            } else {
+                // Singleと同じようにエラーを返す
+                emitter.onError(Throwable())
+            }
+        }
+    }
+}
